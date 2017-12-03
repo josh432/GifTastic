e2bf8416c61cc6254ce6a7bbf18dad685f66c55f
Josh Silverman <jrsilverman71@hotmail.com>
2017-12-03 13:16:59 -0800

    some styling

--- assets/javascript.js
+++ assets/javascript.js
@@ -87,13 +87,13 @@ $(".gifs").on("click", '.gif', function() {
 var state = $(this).attr('data-state');
 
  if (state === 'still') {
- 	$(this).attr('src', $(this).attr('data-animate'));
+ 		 $(this).attr('src', $(this).attr('data-animate'));
          $(this).attr('data-state', 'animate');
-        } else { 
-          $(this).attr('src', $(this).attr('data-still'));
-          $(this).attr('data-state', 'still');
+        }else{ 
+         $(this).attr('src', $(this).attr('data-still'));
+         $(this).attr('data-state', 'still');
 
 
- }
+		 }
 	});
 });
